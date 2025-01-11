781bc3c09c56a0681241dabfb0ec825302732044
hyeonda02 <alceste951005@naver.com>
2025-01-12 01:25:02 +0900

    ✨ Feat: User 엔티티 추가

---
+++ src/main/java/com/umc7th/a1grade/domain/user/entity/Role.java
@@ -0,0 +1,6 @@
+package com.umc7th.a1grade.domain.user.entity;
+
+public enum Role {
+  ROLE_USER,
+  ROLE_ADMIN
+}
--- src/main/java/com/umc7th/a1grade/domain/user/entity/User.java
+++ src/main/java/com/umc7th/a1grade/domain/user/entity/User.java
@@ -1,3 +1,43 @@
 package com.umc7th.a1grade.domain.user.entity;
 
-public class User {}
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+
+import com.umc7th.a1grade.global.common.BaseTimeEntity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User extends BaseTimeEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "user_id")
+  private Long id;
+
+  @Enumerated(EnumType.STRING)
+  private SocialType socialType;
+
+  @NotNull private String socialId;
+  @NotNull private String email;
+
+  @Enumerated(EnumType.STRING)
+  private Role role;
+
+  private String birth;
+  private String region;
+  private String phone;
+  private String nickName;
+}
