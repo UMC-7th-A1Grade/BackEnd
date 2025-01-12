bd6f862ab71b56f316a19892c192431138ffd211
Kim So Yeon <qws1566@naver.com>
2025-01-12 10:14:17 +0900

    :sparkles: 문제 관련 엔티티 생성

--- src/main/java/com/umc7th/a1grade/domain/question/entity/mapping/QuestionLog.java
+++ src/main/java/com/umc7th/a1grade/domain/question/entity/mapping/QuestionLog.java
@@ -1,4 +1,51 @@
 package com.umc7th.a1grade.domain.question.entity.mapping;
 
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.Comment;
+
+import com.umc7th.a1grade.domain.question.entity.Question;
+import com.umc7th.a1grade.domain.user.entity.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
 public class QuestionLog {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long userQuestionId;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "userId", nullable = false)
+	private User user;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "questionId", nullable = false)
+	private Question question;
+
+	@Column(nullable = false)
+	@Comment("제출시간")
+	private LocalDateTime submissionTime;
+
+	@Column(nullable = true)
+	@Comment("메모")
+	private String memo;
+
+	@Column(nullable = false)
+	@Comment("정답 여부")
+	private boolean isCorrect;
 }
--- src/main/java/com/umc7th/a1grade/domain/question/entity/mapping/UserQuestion.java
+++ src/main/java/com/umc7th/a1grade/domain/question/entity/mapping/UserQuestion.java
@@ -1,4 +1,37 @@
 package com.umc7th.a1grade.domain.question.entity.mapping;
 
+import com.umc7th.a1grade.domain.question.entity.Question;
+import com.umc7th.a1grade.domain.user.entity.User;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access= AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 public class UserQuestion {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "question_id", nullable = false)
+	private Question question;
+
 }
