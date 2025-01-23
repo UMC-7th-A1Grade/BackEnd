e403ea2e040371060a24d0c0e257f3a1490ed919
joonyee <ssugjoon@skuniv.ac.kr>
2025-01-24 03:26:52 +0900

    ✨ FEAT: 이미지 저장을 위한 S3 기능 구현(미확정)

---
+++ src/main/java/com/umc7th/a1grade/global/config/S3Config.java
@@ -0,0 +1,60 @@
+package com.umc7th.a1grade.global.config;
+
+import jakarta.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+import lombok.Getter;
+
+@Getter
+@Configuration
+public class S3Config {
+
+  private AWSCredentials awsCredentials;
+
+  @Value("${aws.accessKeyId}")
+  private String accessKey;
+
+  @Value("${aws.secretAccessKey}")
+  private String secretKey;
+
+  @Value("${aws.region.static}")
+  private String region;
+
+  @Value("${aws.s3.bucket}")
+  private String bucket;
+
+  @Value("${aws.s3.path.question}")
+  private String questionPath;
+
+  @Value("${aws.s3.path.memo}")
+  private String memoPath;
+
+  @PostConstruct
+  public void init() {
+    this.awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+  }
+
+  @Bean
+  public AmazonS3 amazonS3() {
+    AWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+    return AmazonS3ClientBuilder.standard()
+        .withRegion(region)
+        .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+        .build();
+  }
+
+  @Bean
+  public AWSCredentialsProvider awsCredentialsProvider() {
+    return new AWSStaticCredentialsProvider(awsCredentials);
+  }
+}
--- src/main/java/com/umc7th/a1grade/global/s3/S3Service.java
+++ src/main/java/com/umc7th/a1grade/global/s3/S3Service.java
@@ -1,3 +1,63 @@
 package com.umc7th.a1grade.global.s3;
 
-public class S3Service {}
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.SdkClientException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.umc7th.a1grade.global.config.S3Config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class S3Service {
+
+  private final AmazonS3 amazonS3;
+
+  private final S3Config s3Config;
+
+  private final UuidRepository uuidRepository;
+
+  public String uploadFile(String keyName, MultipartFile file) {
+    ObjectMetadata metadata = new ObjectMetadata();
+    metadata.setContentLength(file.getSize());
+    try {
+      amazonS3.putObject(
+          new PutObjectRequest(s3Config.getBucket(), keyName, file.getInputStream(), metadata));
+    } catch (IOException e) {
+      log.error("error at AmazonS3Manager uploadFile : {}", (Object) e.getStackTrace());
+    }
+
+    return amazonS3.getUrl(s3Config.getBucket(), keyName).toString();
+  }
+
+  public String generateQuestionKeyName(Uuid uuid) {
+    return s3Config.getQuestionPath() + '/' + uuid.getUuid();
+  }
+
+  public String generateMemoKeyName(Uuid uuid) {
+    return s3Config.getMemoPath() + '/' + uuid.getUuid();
+  }
+
+  public boolean deleteFile(String keyName) {
+    try {
+      amazonS3.deleteObject(new DeleteObjectRequest(s3Config.getBucket(), keyName));
+      log.info("File deleted successfully from S3: {}", keyName);
+      return true;
+    } catch (AmazonServiceException e) {
+      log.error("Error deleting file from S3: {}", e.getErrorMessage());
+    } catch (SdkClientException e) {
+      log.error("Error deleting file from S3: {}", e.getMessage());
+    }
+    return false;
+  }
+}
