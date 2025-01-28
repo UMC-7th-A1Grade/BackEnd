87aef77e7d310fd3c2041f1e8a17ed2758f653a2
hyeonda02 <alceste951005@naver.com>
2025-01-29 02:16:59 +0900

    ✅ Test : 닉네임 중복 확인 API unit test 코드 추가

---
+++ src/test/java/com/umc7th/a1grade/unittest/user/service/UserServiceTest.java
@@ -0,0 +1,80 @@
+package com.umc7th.a1grade.unittest.user.service;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.umc7th.a1grade.domain.user.entity.Role;
+import com.umc7th.a1grade.domain.user.entity.User;
+import com.umc7th.a1grade.domain.user.exception.UserHandler;
+import com.umc7th.a1grade.domain.user.repository.UserRepository;
+import com.umc7th.a1grade.domain.user.service.UserService;
+import com.umc7th.a1grade.domain.user.service.UserServiceImpl;
+import com.umc7th.a1grade.unittest.user.fake.FakeUserDetails;
+import com.umc7th.a1grade.unittest.user.fake.FakeUserRepository;
+
+public class UserServiceTest {
+  private UserService userService;
+  private UserRepository fakeUserRepository;
+  private final String socialId = "user1234";
+  private User existingUser;
+
+  @BeforeEach
+  void init() {
+    fakeUserRepository = new FakeUserRepository();
+    userService = new UserServiceImpl(fakeUserRepository);
+
+    this.existingUser =
+        User.builder()
+            .socialId(socialId)
+            .email("existing-test-email")
+            .role(Role.ROLE_USER)
+            .nickName("existing-nickname")
+            .build();
+
+    fakeUserRepository.save(existingUser);
+  }
+
+  @Test
+  @DisplayName("[confirmNickName] - 정상 요청")
+  void confirmNickName_Success() {
+    /// given
+    String nickName = "new-nickname";
+    String socialId = "new-socialID";
+    UserDetails userDetails = new FakeUserDetails(socialId);
+
+    // when
+    // then
+    assertDoesNotThrow(() -> userService.confirmNickName(userDetails, nickName));
+  }
+
+  @Test
+  @DisplayName("[confirmNickName] - 닉네임을 입력하지 않거나, 공백 입력시 예외 발생")
+  void confirmNickName_invalidInput() {
+    // given
+    String nickName = "   ";
+    String socialId = "new-socialId";
+    UserDetails userDetails = new FakeUserDetails(socialId);
+
+    // when
+    // then
+    assertThrows(UserHandler.class, () -> userService.confirmNickName(userDetails, nickName));
+  }
+
+  @Test
+  @DisplayName("[confirmNickName] - 이미 사용중인 닉네임이라면 예외 발생")
+  void confirmNickName_usedInput() {
+    // given
+    String nickName = "existing-nickname";
+    String socialId = "new-socialId";
+    UserDetails userDetails = new FakeUserDetails(socialId);
+
+    // when
+    // then
+    assertThrows(UserHandler.class, () -> userService.confirmNickName(userDetails, nickName));
+  }
+}
