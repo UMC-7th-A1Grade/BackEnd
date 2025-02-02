e520401a129c452065523b19679371c33f22191f
Kim So Yeon <qws1566@naver.com>
2025-02-03 01:53:26 +0900

    :recycle: refactor: 저장한 문제가 없을 경우 빈 배열 반환

--- src/main/java/com/umc7th/a1grade/domain/question/service/QuestionServiceImpl.java
+++ src/main/java/com/umc7th/a1grade/domain/question/service/QuestionServiceImpl.java
@@ -1,5 +1,6 @@
 package com.umc7th.a1grade.domain.question.service;
 
+import java.util.Collections;
 import java.util.List;
 
 import org.springframework.dao.DataAccessException;
@@ -42,16 +43,10 @@ public class QuestionServiceImpl implements QuestionService {
 
     List<Question> RecentQuestions = questionRepository.findRecentQuestion(user.getId());
 
-    if (RecentQuestions.isEmpty()) {
-      throw new GeneralException(QuestionErrorStatus.QUESTION_NOT_FOUND);
-    }
-
-    if (RecentQuestions.size() < 5) {
-      throw new GeneralException(QuestionErrorStatus.INSUFFICENT_QUESTIONS);
-    }
-
     List<QuestionResponseDTO.QuestionDTO> questionDTOList =
-        questionConverter.toQuestionDTO(RecentQuestions);
+        RecentQuestions.isEmpty()
+            ? Collections.emptyList()
+            : questionConverter.toQuestionDTO(RecentQuestions);
 
     return questionConverter.randomQuestionDTO(questionDTOList);
   }
